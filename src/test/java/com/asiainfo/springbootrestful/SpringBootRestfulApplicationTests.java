0b3cfd99c19b488e5098277af8f6b9649ea472c8
xuezhinan <2236199776@qq.com>
2019-03-19 16:39:25 +0800

    线程测试，错误页面定制

--- src/test/java/com/asiainfo/springbootrestful/SpringBootRestfulApplicationTests.java
+++ src/test/java/com/asiainfo/springbootrestful/SpringBootRestfulApplicationTests.java
@@ -5,6 +5,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.security.RunAs;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootRestfulApplicationTests {
@@ -13,4 +17,36 @@ public class SpringBootRestfulApplicationTests {
     public void contextLoads() {
     }
 
+    //线程测试
+    @Test
+    public void ThreadTest(){
+        for (int i = 0 ; i < 5 ; i++){
+            new Thread(new TaskA()).start();
+        }
+        System.out.println("等待结束");
+    }
+
+    //线程测试
+    @Test
+    public void ThreadTest2(){
+        //SingleThreadExecutor创建的线程会按被提交的顺序单个顺序执行
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        for (int i = 0 ; i < 5 ; i++){
+            service.execute(new TaskA());
+        }
+        service.shutdown();
+        System.out.println("等待结束");
+    }
+
+    //线程测试
+    @Test
+    public void ThreadTest3(){
+
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        for (int i = 0 ; i < 5 ; i++){
+            service.execute(new TaskA());
+        }
+        service.shutdown();
+        System.out.println("等待结束");
+    }
 }
