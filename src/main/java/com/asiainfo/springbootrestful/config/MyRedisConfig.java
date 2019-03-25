9467106f1f2cf9a4afb343d27f3962b6ffc19fb0
xuezhinan <2236199776@qq.com>
2019-03-25 18:07:23 +0800

    整合mybatis

---
+++ src/main/java/com/asiainfo/springbootrestful/config/MyRedisConfig.java
@@ -0,0 +1,25 @@
+package com.asiainfo.springbootrestful.config;
+
+import com.asiainfo.springbootrestful.entities.UserRole;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class MyRedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, Object> myRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setDefaultSerializer(new Jackson2JsonRedisSerializer<UserRole>(UserRole.class));
+        return template;
+    }
+}
