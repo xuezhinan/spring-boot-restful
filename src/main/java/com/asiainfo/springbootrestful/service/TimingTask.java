72360996f2d9f25e7e2152b5b362f34f12509755
xuezhinan <xuezn@maitewang.com>
2019-10-11 15:36:56 +0800

    定时任务注解

--- src/main/java/com/asiainfo/springbootrestful/service/TimingTask.java
+++ src/main/java/com/asiainfo/springbootrestful/service/TimingTask.java
@@ -8,8 +8,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class TimingTask {
 
-    @Async // 这是一个异步处理的方法
-    //@Scheduled(fixedDelay = 10000)  //定时任务的注解,单位是毫秒
+    @Async // 这是一个异步处理的方法，多线程执行异步任务
+    /**fixedRate:上一次开始执行时间点之后5秒再执行*/
+    //@Scheduled(fixedRate = 10000)  //定时任务的注解,单位是毫秒
+    /**fixedDelay:上一次执行完毕时间点之后5秒再执行*/
+    //@Scheduled(fixedDelay = 5000)
+    //@Scheduled(cron = "* * * * * * *")
+    /**第一次延迟2秒后执行，之后按fixedDelay的规则每5秒执行一次*/
+    //@Scheduled(initialDelay = 2000, fixedDelay = 5000)
     public void dealSomething() throws Exception{
         System.out.println("执行之前的时间是："+System.currentTimeMillis());
         //模拟调用接口耗时
